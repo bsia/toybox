1efd2583b42dd47ea88df2b64f9e11b1a2e5e588
Barry Sia <barry.sia@gmail.com>
2014-03-11 12:42:15 -0700

    Fix issue with getdelim
    * Also set line variable whenever a realloc needs to be done.  Previous
    code crashes whenever the data is larger than 1024.

--- lib/portability.c
+++ lib/portability.c
@@ -34,7 +34,7 @@ ssize_t getdelim(char **linep, size_t *np, int delim, FILE *stream)
       new_line = realloc(*linep, new_len);
       if (!new_line) return -1;
       *np = new_len;
-      *linep = new_line;
+      line = *linep = new_line;
     }
 
     line[i] = ch;
@@ -48,7 +48,7 @@ ssize_t getdelim(char **linep, size_t *np, int delim, FILE *stream)
     new_line = realloc(*linep, new_len);
     if (!new_line) return -1;
     *np = new_len;
-    *linep = new_line;
+    line = *linep = new_line;
   }
   line[i + 1] = '\0';
 
